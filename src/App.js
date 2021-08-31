2220440ba7f4e1441c9e51653a56345908b7e8c0
gon-za <gonzalopalacio0101@gmail.com>
2021-08-31 15:09:45 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
+  import React from 'react';
+  import ReactDOM form 'react-dom';
+  import NavBar from './components/NavBar.js';
   import './App.css';
-  import fondo from '../public/fondo.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <title>Noti Futbol</title>
+        <NavBar />
       </header>
-      <table>
-        <tr>
-          <td>
-            <img src={fondo} alt="fondo" />
-          </td>
-          <td></td>
-          <td></td>
-        </tr>
-      </table>
 
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/NavBar.sass';
+
+const NavBar = () => {
+    return (
+        <div className="title">
+            <h1>Titleeee</h1>
+            
+        </div>
+    )
+}
+
+export default NavBar;
